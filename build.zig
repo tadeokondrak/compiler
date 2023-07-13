69323c5e377e0272b4a58945a1ae5a9924fd271d
Tadeo Kondrak <me@tadeo.ca>
2023-07-13 14:37:44 -0600

    don't build the lsp for the run step

--- build.zig
+++ build.zig
@@ -52,7 +52,7 @@ pub fn build(b: *std.Build) void {
 
     const run_cmd = b.addRunArtifact(exe);
 
-    run_cmd.step.dependOn(b.getInstallStep());
+    run_cmd.step.dependOn(&exe.step);
     if (b.args) |args|
         run_cmd.addArgs(args);
     const run_step = b.step("run", "Run the app");
